1ad2774fa54e09bdf31b6624720759e4c58957a8
Nicholas Facciola <nicholas.facciola@gmail.com>
2025-04-03 16:41:50 -0700

    basic kcp networking

---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using Mirror;
+
+public class PlayerController : NetworkBehaviour
+{
+    void Update()
+    {
+        if(isLocalPlayer)
+        {
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            Vector3 playerMovement = new Vector3(h * 0.25f, v * 0.25f, 0);
+            transform.position = transform.position + playerMovement;
+        }
+    }
+}
