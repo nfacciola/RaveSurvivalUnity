ff44423a611f9248d825d5396310dcc397af48ec
covmoore <covmoore5@gmail.com>
2025-04-15 07:33:35 -0700

    adding muzzle falshes and fire sound

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -5,23 +5,35 @@ public class Gun : MonoBehaviour
   public float damage = 10f;
   public float range = 100f;
   public float fireRate = 15f;
+  private float nextTimeToFire = 0f;
   public Camera fpsCam;
   public ParticleSystem muzzleFalsh;
   public GameObject impactEffect;
+  private AudioSource audioSource;
+  public AudioClip fireSound;
 
-  private float nextTimeToFire = 0f;
 
+  void Start()
+  {
+    audioSource = GetComponent<AudioSource>();
+  }
 
-    // Update is called once per frame
-    void Update()
+  // Update is called once per frame
+  void Update()
     {
+      
         if(Input.GetButton("Fire1") && Time.time >= nextTimeToFire) {
-          nextTimeToFire = Time.time + 1f/fireRate;
+          Debug.Log(1f/fireRate);
+          nextTimeToFire = Time.time + (1f/fireRate);
           Shoot();
         }
     }
 
     void Shoot() {
+      if(audioSource.clip == null || audioSource != fireSound) {
+        audioSource.clip = fireSound;
+      }
+      audioSource.Play();
       muzzleFalsh.Play();
       RaycastHit hit;
       if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range)) {
