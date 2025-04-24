1ceac728772a8fbef98e03b02351484bb4fc52a3
covmoore <covmoore5@gmail.com>
2025-04-23 22:22:50 -0700

    gave enemies an ai sensor with sight detection

--- Assets/AISensor.cs
+++ Assets/AISensor.cs
@@ -1,28 +1,84 @@
+using System.Collections;
+using System.Threading;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering;
 
+//[ExecuteInEditMode]
 public class AISensor : MonoBehaviour
 {
+  private Enemy enemy;
   public float distance = 10f;
   public float angle = 30f;
   public float height = 1.0f;
   public Color meshColor = Color.red;
+
+  int count;
+  Collider[] colliders = new Collider[20];
+  public LayerMask layers;
+  public LayerMask obstructionMask;
+
+  private bool canSeePlayer = false;
   Mesh mesh;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+      enemy = gameObject.GetComponentInParent<Enemy>();
+      StartCoroutine(ScanRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private IEnumerator ScanRoutine() {
+      WaitForSeconds wait = new WaitForSeconds(0.2f);
+
+      while (true) {
+        yield return wait;
+        Scan();
+      }
+    }
+
+    private void Scan() {
+      count = Physics.OverlapSphereNonAlloc(transform.position, distance, colliders, layers, QueryTriggerInteraction.Collide);
+      for(int i = 0; i < count; ++i) {
+        GameObject target = colliders[i].gameObject;
+        if(IsInSight(target) && target.layer == LayerMask.NameToLayer("Player")) {
+          canSeePlayer = true;
+          enemy.PlayerSpotted(target.transform);
+        } else {
+          canSeePlayer = false;
+          enemy.NoPlayerFound();
+        }
+      }
+    }
+
+    private bool IsInSight(GameObject target) {
+      
+      Vector3 origin = transform.position;
+      Vector3 dest = target.transform.position;
+      Vector3 direction = dest - origin;
+      if(direction.y < 0 || direction.y > height) {
+        return false;
+      }
+
+      direction.y = 0;
+      float deltaAngle = Vector3.Angle(direction, transform.forward);
+      if(deltaAngle > angle) {
+        return false;
+      }
+
+      Vector3 directionToTarget = (target.transform.position - transform.position).normalized;
+      float distanceToTarget = Vector3.Distance(transform.position, target.transform.position);
+      if(Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask)) {
+        return false;
+      }
+
+      return true;
     }
 
     Mesh CreateWedgeMesh() {
       Mesh mesh = new Mesh();
 
-      int numTriangles = 8;
+      int segments = 10;
+      int numTriangles = (segments * 4) + 2 + 2;
       int numVertices = numTriangles * 3;
 
       Vector3[] vertices = new Vector3[numVertices];
@@ -56,25 +112,40 @@ public class AISensor : MonoBehaviour
       vertices[vert++] = bottomRight;
       vertices[vert++] = bottomCenter;
 
-      //far side
-      vertices[vert++] = bottomLeft;
-      vertices[vert++] = bottomRight;
-      vertices[vert++] = topRight;
+      float currentAngle = -angle;
+      float deltaAngle = (angle * 2) / segments;
+      for(int i = 0; i < segments; ++i) {
+        
+        bottomLeft = Quaternion.Euler(0, currentAngle, 0) * Vector3.forward * distance;
+        bottomRight = Quaternion.Euler(0, currentAngle + deltaAngle, 0) * Vector3.forward * distance;
 
-      vertices[vert++] = topRight;
-      vertices[vert++] = topLeft;
-      vertices[vert++] = bottomLeft;
+        topRight = bottomRight + Vector3.up * height;
+        topLeft = bottomLeft + Vector3.up * height;
 
-      //top
-      vertices[vert++] = topCenter;
-      vertices[vert++] = topLeft;
-      vertices[vert++] = topRight;
+        //far side
+        vertices[vert++] = bottomLeft;
+        vertices[vert++] = bottomRight;
+        vertices[vert++] = topRight;
 
-      //bottom
-      vertices[vert++] = bottomCenter;
-      vertices[vert++] = bottomRight;
-      vertices[vert++] = bottomLeft;
+        vertices[vert++] = topRight;
+        vertices[vert++] = topLeft;
+        vertices[vert++] = bottomLeft;
 
+        //top
+        vertices[vert++] = topCenter;
+        vertices[vert++] = topLeft;
+        vertices[vert++] = topRight;
+
+        //bottom
+        vertices[vert++] = bottomCenter;
+        vertices[vert++] = bottomRight;
+        vertices[vert++] = bottomLeft;
+
+        
+        currentAngle += deltaAngle;
+      }
+
+     
       for(int i = 0; i < numVertices; ++i) {
         triangles[i] = i;
       }
@@ -86,10 +157,11 @@ public class AISensor : MonoBehaviour
       return mesh;
     }
 
+    
+
   private void OnValidate()
   {
     mesh = CreateWedgeMesh();
-    Debug.Log("VALIDATE");
   }
 
   private void OnDrawGizmos()
@@ -97,6 +169,15 @@ public class AISensor : MonoBehaviour
       if(mesh) {
         Gizmos.color = meshColor;
         Gizmos.DrawMesh(mesh, transform.position, transform.rotation);
-      }  
+      }
+    Gizmos.DrawWireSphere(transform.position, distance);
+    if(canSeePlayer) {
+      Gizmos.color = new Color(0,1,0,0.5f);
+    } else {
+      Gizmos.color = new Color(0,0,1,0.5f);
+    }
+    for(int i =0; i < count; ++i) {
+      Gizmos.DrawSphere(colliders[i].transform.position, 1f);
+    }
   }
 }
--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -7,41 +7,56 @@ public class Enemy : MonoBehaviour
 {
     public float health = 50f;
     public NavMeshAgent agent;
-    public ArrayList playerTansforms = new ArrayList();
-    public Transform playerPos;
+    //public ArrayList playerTansforms = new ArrayList();
+    //public Transform playerPos;
+    private Transform target = null;
+    private IEnumerator chasingCo = null;
 
   public void Start()
   {
     agent = GetComponent<NavMeshAgent>();
-    var playerMoves = FindObjectsByType<PlayerMove>(FindObjectsSortMode.None);
-    foreach(PlayerMove player in playerMoves) {
-      var temp = player.GetComponentInParent<Transform>();
-      if (temp != null) {
-        playerTansforms.Add(temp);
-      }
-    }
+    // var playerMoves = FindObjectsByType<PlayerMove>(FindObjectsSortMode.None);
+    // foreach(PlayerMove player in playerMoves) {
+    //   var temp = player.GetComponentInParent<Transform>();
+    //   if (temp != null) {
+    //     playerTansforms.Add(temp);
+    //   }
+    // }
   }
 
   public void Update()
   {
-    float min = 99999999;
-    foreach(Transform player in playerTansforms) {
-      float dist = Vector3.Distance(player.transform.position, GetComponent<Transform>().transform.position);
-      if(dist < min) {
-        min = dist;
-        playerPos = player;
-      }
-    }
-    MoveToPlayer();
+    // float min = 99999999;
+    // foreach(Transform player in playerTansforms) {
+    //   float dist = Vector3.Distance(player.transform.position, GetComponent<Transform>().transform.position);
+    //   if(dist < min) {
+    //     min = dist;
+    //     playerPos = player;
+    //   }
+    // }
   }
 
-  void MoveToPlayer() {
-    if(playerPos != null) {
-      //agent.CalculatePath(playerPos.transform.position, new NavMeshPath());
-      transform.LookAt(playerPos);
-      agent.SetDestination(playerPos.position);
+  public void PlayerSpotted(Transform player) {
+    if(target != player) {
+      target = player;
+      chasingCo = MoveToPlayer(player);
+      StartCoroutine(chasingCo);
     }
-    
+  }
+  public void NoPlayerFound() {
+    if(chasingCo != null) {
+      StopCoroutine(chasingCo);
+      target = null;
+    }
+  }
+
+  IEnumerator MoveToPlayer(Transform player) {
+    float wait = 0.2f;
+    while(true) {
+      transform.LookAt(player);
+      agent.SetDestination(player.position);
+      yield return wait;
+    }
   }
 
   public void TakeDamage(float dmg) {
