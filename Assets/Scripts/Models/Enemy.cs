d315f1a95b44f116641ff0f662fc1736ed9b26ed
Nicholas Facciola <nicholas.facciola@gmail.com>
2025-04-28 12:10:05 -0700

    commit before deleting old player controller

---
+++ Assets/Scripts/Models/Enemy.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace RaveSurvival
+{
+  public class Enemy : MonoBehaviour
+  {
+      public float health = 50f;
+
+      public void TakeDamage(float dmg) {
+        health -= dmg;
+        if(health <= 0f) {
+          Die();
+        }
+      }
+
+      void Die() {
+        Destroy(gameObject);
+      }
+  }
+}
