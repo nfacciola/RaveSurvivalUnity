76378d6ef1d2d0c8bab1cd91a35f30c4ee35199f
Nicholas Facciola <nicholas.facciola@gmail.com>
2025-05-10 14:53:29 -0700

    spawn manager

---
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using Mirror;
+using System.Collections.Generic;
+
+public class SpawnManager : NetworkBehaviour
+{
+    public GameObject enemyPrefab;
+    public Transform spawnPointParent;
+    public List<Transform> spawnPoints = new List<Transform>();
+
+    void Start()
+    {
+        for(int i = 0; i < spawnPointParent.childCount; i++)
+        {
+            spawnPoints.Add(spawnPointParent.GetChild(i).transform);
+        }
+        if(isServer)
+        {
+            SpawnEnemies();
+        }   
+    }
+
+    void SpawnEnemies()
+    {
+        foreach(Transform spawnPoint in spawnPoints)
+        {
+            GameObject enemy = Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+            NetworkServer.Spawn(enemy);
+        }
+    }
+}
