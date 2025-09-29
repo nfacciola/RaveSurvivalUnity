24c4eecdb4b193165b8d30f57ccc101ed4463f3a
covmoore <covmoore5@gmail.com>
2025-09-28 20:41:12 -0700

    creating initial spawns with spawn manager

--- Assets/Scripts/Models/Spawn.cs
+++ Assets/Scripts/Models/Spawn.cs
@@ -1,51 +1,57 @@
-// using UnityEngine;
+using UnityEngine;
+using RaveSurvival;
+using System.Collections;
+using Mirror;
 
-// namespace RaveSurvival
-// {
-//     public class Spawn : MonoBehaviour
-//     {
-//         public enum SpawnType
-//         {
-//             spawnPoint = 0,
-//             spawnArea
-//         }
+public class Spawn : MonoBehaviour
+{
+    public enum SpawnType
+    {
+        spawnPoint = 0,
+        spawnArea
+    }
 
-//         public enum SpawnUser
-//         {
-//             player = 0,
-//             enemy,
-//             boss
-//         }
+    public enum SpawnUser
+    {
+        player = 0,
+        enemy,
+        boss
+    }
 
-//         public SpawnType spawnType = SpawnType.spawnPoint;
-//         public SpawnUser spawnUser = SpawnUser.enemy;
-//         public float radius = 0f;
+    public SpawnType spawnType = SpawnType.spawnPoint;
+    public SpawnUser spawnUser = SpawnUser.enemy;
+    public float radius = 0f;
 
-//         public void Spawn(Gameobject[] entities, float delay, float rate)
-//         {
-//             if (spawnType == SpawnType.spawnPoint)
-//             {
+    public void SpawnCharacters(GameObject[] entities, float delay = 0.0f, float spwnRate = 0.0f)
+    {
+        IEnumerator spawn = SpawnEntities(entities, delay, spwnRate);
+        StartCoroutine(spawn);
+    }
 
-//             }
+    public SpawnUser GetSpawnUser()
+    {
+        return spawnUser;
+    }
 
+    private IEnumerator SpawnEntities(GameObject[] entities, float delay, float spwnRate)
+    {
+        // wait delay
+        yield return new WaitForSeconds(delay);
 
-//         }
-//         public IEnumerator SpawnEntities(Gameobject[] entities, float delay, float rate)
-//         {
-//             // wait delay
-//             yield return new WaitForSeconds(delay);
+        foreach (GameObject entity in entities)
+        {
+            SpawnEntity(entity);
+            yield return new WaitForSeconds(spwnRate);
+        }
+    }
 
-//             foreach (Gameobject entity in entities)
-//             {
-//                 SpawnEntity();
-//                 yield return new WaitForSeconds(rate);
-//             }
-//             StopCoroutine();
-//         }
-
-//         private void SpawnEntity(GameObject entity)
-//         {
-
-//         }
-//     }
-// }
+    private void SpawnEntity(GameObject entity)
+    {
+        GameObject character = Instantiate(entity, transform.position, transform.rotation);
+        Debug.Log("Created entity: " + character.name);
+        if (GameManager.Instance.gameType == GameManager.GameType.OnlineMultiplayer)
+        {
+            NetworkServer.Spawn(character);
+        }
+    }
+}
